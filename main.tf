f7e705239cff7da631797df1150ae6e3b2cfc904
ShadowB1te <thomas.hansen.muri@proton.me>
2024-11-27 14:42:16 +0100

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = "1.10.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+  cloud {
+    organization = "thomas-muri-organization-1"
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "thomas-muri-resource-group-1"
+  location = "northeurope"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "tmstgacc1"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+}
